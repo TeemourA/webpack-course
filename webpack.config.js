3a4ddc4ed213255265ec78e56a62723815f6ae07
Ахмедов Тимур Орханович <takhmedov@phoenixit.ru>
2020-12-18 16:26:43 +0300

    add custom font

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,10 @@ module.exports = {
         test: /\.(png|jpg|svg|gif)$/,
         use: ['file-loader'],
       },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        use: ['file-loader'],
+      },
     ],
   },
 };
